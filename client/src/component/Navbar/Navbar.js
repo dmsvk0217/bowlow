cac8f1c782767f7828f4f61344e207246ce82b40
dmsvk0217 <dmsvk0217@gmail.com>
2022-12-19 06:02:26 +0900

    mobile navbar 구현

--- client/src/component/Navbar/Navbar.js
+++ client/src/component/Navbar/Navbar.js
@@ -4,14 +4,24 @@ import "./Navber.css";
 
 function Navbar() {
   const positionChange = useRef();
+  const opacityChange = useRef();
+  const navBarChange = useRef();
 
-  const menuHandler = () => {
-    // ChangePosition.current.style = "right:0px;";
+  const menuOnHandler = () => {
+    positionChange.current.style =
+      "display:block; visibility: visible; right:0px; z-index:101";
+    opacityChange.current.style =
+      "opacity:0.5; visibility: visible; right: 300px; z-index:100";
+  };
+
+  const menuOffHandler = () => {
+    positionChange.current.style = "right:-300px; z-index:-1";
+    opacityChange.current.style = "opacity:0;  right: 0px; z-index:-1";
   };
 
   return (
     <>
-      <nav className="navbar">
+      <nav className="navbar" ref={navBarChange}>
         <div className="navbar__top">
           <div className="navbar__top__logo">
             <span>BOWLOW</span>
@@ -41,6 +51,9 @@ function Navbar() {
               <Link to="#">장바구니</Link>
             </li>
           </ul>
+          <Link to="#" className="menu" onClick={menuOnHandler}>
+            MENU
+          </Link>
         </div>
 
         <ul className="navbar__bottom">
@@ -57,35 +70,59 @@ function Navbar() {
             <Link to="#">SHOP</Link>
           </li>
         </ul>
-        <Link to="#" className="menu" onClick={menuHandler}>
-          MENU
-        </Link>
       </nav>
       <div ref={positionChange} className="mobile__menu">
         <ul className="mobile__menu__section__1">
-          <li className="mobile__menu__section__1__item">검색</li>
-          <li className="mobile__menu__section__1__item">로그인</li>
-          <li className="mobile__menu__section__1__item">장바구니</li>
-          <li className="mobile__menu__section__1__item">마이 페이지</li>
+          <li className="mobile__menu__section__1__item">
+            <Link to="#">검색</Link>
+          </li>
+          <li className="mobile__menu__section__1__item">
+            <Link to="#">로그인</Link>
+          </li>
+          <li className="mobile__menu__section__1__item">
+            <Link to="#">장바구니</Link>
+          </li>
+          <li className="mobile__menu__section__1__item">
+            <Link to="#">마이 페이지</Link>
+          </li>
         </ul>
 
         <ul className="mobile__menu__section__2">
-          <li className="mobile__menu__section__2__item">BOWUL</li>
-          <li className="mobile__menu__section__2__item">NEW ARRIVALS</li>
-          <li className="mobile__menu__section__2__item">BEST ITEM</li>
-          <li className="mobile__menu__section__2__item">SHOP</li>
+          <li className="mobile__menu__section__2__item">
+            <Link to="#">BOWUL</Link>
+          </li>
+          <li className="mobile__menu__section__2__item">
+            <Link to="#">NEW ARRIVALS</Link>
+          </li>
+          <li className="mobile__menu__section__2__item">
+            <Link to="#">BEST ITEM</Link>
+          </li>
+          <li className="mobile__menu__section__2__item">
+            <Link to="#">SHOP</Link>
+          </li>
         </ul>
 
         <ul className="mobile__menu__section__3">
-          <li className="mobile__menu__section__3__item">공지사항</li>
-          <li className="mobile__menu__section__3__item">이벤트</li>
-          <li className="mobile__menu__section__3__item">리뷰</li>
+          <li className="mobile__menu__section__3__item">
+            <Link to="#">공지사항</Link>
+          </li>
+          <li className="mobile__menu__section__3__item">
+            <Link to="#">이벤트</Link>
+          </li>
+          <li className="mobile__menu__section__3__item">
+            <Link to="#">리뷰</Link>
+          </li>
         </ul>
 
         <ul className="mobile__menu__section__4">
           <Link to="#">Instagram</Link>
         </ul>
       </div>
+      <div
+        className="mobile__box"
+        ref={opacityChange}
+        onClick={menuOffHandler}
+      ></div>
     </>
   );
 }
