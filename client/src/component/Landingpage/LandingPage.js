04488f4f8bc721c77dc10c1f012bfa0de78d741d
dmsvk0217 <dmsvk0217@gmail.com>
2022-12-19 07:23:15 +0900

    landing page layout 구현

--- client/src/component/Landingpage/LandingPage.js
+++ client/src/component/Landingpage/LandingPage.js
@@ -1,11 +1,28 @@
 import React from "react";
 import "./LandingPage.css";
+import landingImage from "../img/LandingImg.png";
 
 function LandingPage() {
   return (
     <>
       <div className="landing">
-        <p>LandingPage</p>
+        <img className="landingImg" src={landingImage} alt="LandingIMG" />
+        <div className="land">
+          <div className="grid">
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+          </div>
+        </div>
       </div>
     </>
   );
