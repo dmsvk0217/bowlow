bbedeb19b5ce948bb17a256bd78fd4f5c5cef6f1
dmsvk0217 <dmsvk0217@gmail.com>
2022-12-24 22:40:07 +0900

    test

--- server/models/cart.model.js
+++ server/models/cart.model.js
@@ -5,5 +5,6 @@ const Cart = function (cart) {
   this.user_id = cart.user_id;
   this.product_id = cart.product_id;
   this.product_option_id = cart.product_option_id;
+
   this.quantity = cart.quantity;
 };
